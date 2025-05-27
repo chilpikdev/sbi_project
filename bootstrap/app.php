b8b0a4d86b13b84da7dd5a82bd7f6c3ae063e9bc
i.xaliev <i.xaliev@uzinfocom.uz>
2025-05-27 19:05:24 +0500

    ref: updated app file & created middleware for accepting only json in headers for api route group

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +14,25 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // $middleware->alias([
+        //     'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        // ]);
+        $middleware->group('api', [
+            \App\Http\Middleware\ForceJsonResponse::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (AuthenticationException $ex) {
+            return response()->json([
+                'status' => 401,
+                'message' => __('auth.unauthorized'),
+            ], 401);
+        });
+
+        $exceptions->render(function (HttpException $ex) {
+            return response()->json([
+                'status' => $ex->getStatusCode(),
+                'message' => $ex->getMessage(),
+            ], $ex->getStatusCode());
+        });
     })->create();
